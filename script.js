0db905ebfc31bda8dcf28a404c75453f6ab13603
ManjulaKFrontend <148309702+ManjulaKFrontend@users.noreply.github.com>
2024-03-02 00:11:14 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+var box1=document.getElementById("num1")
+var box2=document.getElementById("num2")
+var result=document.getElementById("result")
+
+console.log(total)
+
+
+
+
+
+function myfun(){
+
+var box1value =Number(box1.value)
+var box2value=Number(box2.value)
+var total=box1value+box2value
+result.textContent=total
+
+
+
+}
+
+
+
+
+
+
+
+
+
